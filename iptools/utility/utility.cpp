3f1554b6087a6a016d89f3b6daad1546acbedef1
Andrew Fernandez <fdez26@ymail.com>
2020-04-19 14:49:39 -0400

    trying something diff

--- iptools/utility/utility.cpp
+++ iptools/utility/utility.cpp
@@ -205,7 +205,8 @@ void utility::cv_canny_edge(cv::Mat &src, cv::Mat &tgt, const vector<roi>& regio
 void utility::cv_sobel_edge(cv::Mat &src, cv::Mat &tgt, const vector<roi>& regions) {
 	Mat temp_img;
 	cv_gray(src, temp_img);
-	tgt = temp_img.clone();
+	temp_img.copyTo(tgt);
+	// tgt = temp_img.clone();
 
 	// params for Sobel OpenCV Function
 	int ddepth = -1;
